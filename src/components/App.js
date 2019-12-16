004036413172ad0a3b879b864455d2e8f54ee4e8
María García de Lomana <mariagarciadelomana@gmail.com>
2019-12-16 20:53:14 +0100

    add LandingContent React version

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import '../style/App.scss';
+import Landing from './Landing';
 
 function App() {
   return (
     <div className="App">
+      <Landing />
+
     </div>
   );
 }
