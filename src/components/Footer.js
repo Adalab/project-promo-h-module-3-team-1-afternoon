9e6ab60de0053e6728cf983839c9c593b42f538a
nataliajimenezq <snjimenezq@gmail.com>
2019-12-16 20:26:08 +0100

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = (props) => {
+	return (
+		<footer className="footer">
+			<p className="footer__text">{props.textFooter}</p>
+			<div className="footer__logo">
+				<a href={props.linkFooter} target="_blank">
+					<img className="footer__logo--photo" src={props.logoFooter} />
+				</a>
+			</div>
+		</footer>
+	);
+}
+
+
+export default Footer;
