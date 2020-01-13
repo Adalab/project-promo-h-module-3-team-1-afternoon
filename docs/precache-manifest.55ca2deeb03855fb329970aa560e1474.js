41047a14949299db3a4c96f3d49ea580666b55e9
María García de Lomana <mariagarciadelomana@gmail.com>
2020-01-13 10:53:31 +0100

    prepare project to be published at GitHub Pages

---
+++ docs/precache-manifest.55ca2deeb03855fb329970aa560e1474.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15e98c9563018d1ca90a0f4463d69608",
+    "url": "./index.html"
+  },
+  {
+    "revision": "88a3d4f86a7f80274ca0",
+    "url": "./static/css/main.a54467ca.chunk.css"
+  },
+  {
+    "revision": "2574778885a271aea0c1",
+    "url": "./static/js/2.237a3f1c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.237a3f1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88a3d4f86a7f80274ca0",
+    "url": "./static/js/main.3257b2d2.chunk.js"
+  },
+  {
+    "revision": "987d7948c00e8e39f325",
+    "url": "./static/js/runtime-main.b8b50163.js"
+  },
+  {
+    "revision": "3772c384e81cbfcd42ad08649aaeb0c6",
+    "url": "./static/media/defaultImage.3772c384.png"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "./static/media/profile.0118e311.svg"
+  }
+]);
