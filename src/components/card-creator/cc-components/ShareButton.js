0f2fccd8d9c8e0a48f7738dd70e0e56c50d33863
María García de Lomana <mariagarciadelomana@gmail.com>
2020-01-16 12:28:49 +0100

    add reusable component shareButton

--- src/components/card-creator/cc-components/ShareButton.js
+++ src/components/card-creator/cc-components/ShareButton.js
@@ -1,31 +1,16 @@
 import React from 'react';
 
-const ShareButton = ({ cardUrl }) => {
-    const twitterText = encodeURIComponent('He creado esta tarjeta con Awesome Profile Cards. ¡Échale un vistazo!');
-    const twitterHashtag = encodeURIComponent('adalab,adalaber,frontEnd,awesomeCards');
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${twitterText}&url=${cardUrl}&hashtags=${twitterHashtag}`;
-    const linkedinUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${cardUrl}`;
-
+const ShareButton = ({ name, url }) => {
     return (
         <>
             <div className="create-button-two form imput submit">
-                <a
-                    className="shareInTwitter"
+                <a className={`${name}-btn`}
                     target="_blank"
                     rel="noopener noreferrer"
-                    href={twitterUrl}>
-                    <i className="logo-button fab fa-twitter"></i>
-                    <span>compartir </span>en twitter
+                    href={url}>
+                    <i className={` fab fa-${name}`}></i>
+                    <span>Compartir</span> en <span>{name}</span>
                 </a>
-            </div>
-            <div className="create-button-two form imput submit">
-                <a
-                    className="linkedin-btn"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href={linkedinUrl}><i
-                        className="fab fa-linkedin"></i>
-                    <span>compartir </span>en linkedin</a>
             </div>
         </>
     )
--- src/components/card-creator/cc-components/ShareMessage.js
+++ src/components/card-creator/cc-components/ShareMessage.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ShareButton from './ShareButton';
 
 const ShareMessage = ({ cardUrl }) => {
+    const twitterText = encodeURIComponent('He creado esta tarjeta con Awesome Profile Cards. ¡Échale un vistazo!');
+    const twitterHashtag = encodeURIComponent('adalab,adalaber,frontEnd,awesomeCards');
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${twitterText}&url=${cardUrl}&hashtags=${twitterHashtag}`;
+    //const linkedinUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${cardUrl}`;
+
     return (
         <div id="share-card" className="active-share create__share-two">
             <h2 className="title-share"><span>la</span> tarjeta ha sido creada</h2>
             <a className="link-share" target="_blank" rel="noopener noreferrer"
                 href={cardUrl}>Ver tarjeta</a>
-            <ShareButton cardUrl={cardUrl} />
+            <ShareButton
+                name={'twitter'}
+                url={twitterUrl} />
+            {/* <ShareButton name={'linkedin'}
+                url={linkedinUrl} /> */}
+
         </div>
     )
 }
